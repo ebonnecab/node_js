98f47eec4243f32c6c04b45ee9160ae4747892c5
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2018-12-17 08:46:54 -0800

    user can see all images

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require ('express')
 const app = express()
 const exphbs = require('express-handlebars')
 
+//Index
 app.get('/', (req, res) => {
-    res.render('home', { msg: 'Handlebars are Cool!'});
+    res.render('images-index', { nasaImages: nasaImages })
 })
 
 app.listen(3000, () => {
@@ -12,3 +13,10 @@ app.listen(3000, () => {
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+//mock array of data
+let nasaImages = [
+    {name: "name", location: "location"},
+    {name: "name2", location: "location2"}
+]
+
