68cae50a6c07bfb542311ce247b8054568d86088
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2018-12-17 13:32:03 -0800

    fixed whitespace

--- app.js
+++ app.js
@@ -13,16 +13,8 @@ app.listen(3000, () => {
 })
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+
 app.set('view engine', 'handlebars');
 
-
 mongoose.connect('mongodb://localhost/nasa-images');
-
-//Model
-
-//mock array of data
-// let nasaImages = [
-//     {date: "YYYY-MM-DD", name: "name"},
-//     { date: "YYYY-MM-DD", name: "name"}
-// ]
 
